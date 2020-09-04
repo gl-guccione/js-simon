6282aa4aa2e156a1460dd30c4e2a627b9a7bb059
gl-guccione <g.guccione3@gmail.com>
2020-09-04 15:42:09 +0200

    done first part

--- js/script.js
+++ js/script.js
@@ -41,19 +41,71 @@ function arrayRandomNumbers(quantityElementGen, minNumGen, maxNumGen) {
 }
 
 
+// function that check if two arrays is equal
+
+function isEqual(array1, array2) {
+
+  if (array1.length != array2.length) {
+    return false;
+  }
+
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] != array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
 // script
 
 // generate 5 random numbers => pcNumbers
-var pcNumbers = arrayRandomNumbers(5, 1, 10);
+var qntNumbers = 5;
+var minNumber = 1;
+var maxNumber = 10;
+var pcNumbers = arrayRandomNumbers(qntNumbers, minNumber, maxNumber);
 
 // alert the 5 numbers to the user
 
+alert("Ciao e benvenuto");
+alert("fra poco ti verranno mostrati 5 numeri");
+alert("memorizzali!!\nDopo 30 secondi dovrai inserirli, uno ad uno, nello stesso ordine");
+
+var listNumbers = "";
+for (var i = 0; i < qntNumbers; i++) {
+  if (i == qntNumbers - 1) {
+    listNumbers += pcNumbers[i].toString() + ".";
+  } else {
+    listNumbers += pcNumbers[i].toString() + ", ";
+  }
+}
+
+alert("I numeri sono: " + listNumbers);
+
+alert("Via ai 30 secondi");
 
 // after 30 seconds ask the user to insert all the number one by one in the same order
 // and save the numbers => array => userNumbers;
 
+setTimeout(function () {
+
+  var userNumbers = [];
+
+  for (var i = 0; i < qntNumbers; i++) {
+    userNumbers.push(parseInt(prompt("Inserisci il primo numero")));
+  }
+
+  // check if the two arrays are equal
+  // if true => alert - hai vinto
+
+  if (isEqual(pcNumbers, userNumbers)) {
+    alert("Complimenti, hai vinto!!!");
+
+    // if false check how many correct numbers the user have insert => alert that number
+  } else {
+
+  }
 
 
-// check if the two arrays are equal
-// if true => alert - hai vinto
-// if false check how many correct numbers the user have insert => alert that number
+}, 3000);
